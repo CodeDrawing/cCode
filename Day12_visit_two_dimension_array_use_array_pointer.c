d7ed8d33aaec9a908c3bcc0813a9b6ad0c20f34b
zx <845391508@qq.com>
2023-03-07 10:47:58 +0800

    Day12

--- Day12_visit_two_dimension_array_use_array_pointer.c
+++ Day12_visit_two_dimension_array_use_array_pointer.c
@@ -4,10 +4,10 @@
 int main()
 {
     int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
-    int (*pa)[3]=a,sum=0;
+    int *pa[3]={a[0],a[1],a[2]},sum=0,row=0;
     for (int j = 0; j < 3; ++j) {
         for (int i = 0; i < 3; ++i) {
-            sum+=*(*(pa+j)+i);
+            sum+=*(pa[j]+i);
         }
     }
 
