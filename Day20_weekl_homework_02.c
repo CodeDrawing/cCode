586a948549c902a282d3b9a348c8f080573b54b8
zx <845391508@qq.com>
2023-03-17 19:40:46 +0800

    Day20

--- Day20_weekl_homework_02.c
+++ Day20_weekl_homework_02.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
-
-
+#include <stdlib.h>
 int main(){
+    int n;
+    printf("please input a numbers: \n");
+    scanf("%d",&n);
+    int (*matrix)[n]=(int *)malloc(sizeof (int)*n);
+    for (int i = 0; i < n; ++i) {
+        /*
+         * 想的屎山代码，还不对，想复杂了
+         */
+//           if(i+j<=n-1){
+//               *(*(matrix+i)+j)=(n-j-i)*(n-j-i);
+//           }else if(n-j-i==0) {
+//               *(*(matrix + i) + j) = n * n;
+//           }else if(i==j){
+//               *(*(matrix+i)+j)=(n-i+1)*(n-i+1);
+//           }else {
+//               *(*(matrix+i)+j)= abs(n-j-i)*abs(n-j-i);
+//           }
 
+        for (int j = 0; j < n; ++j) {
+           if(i+j<=n-1){
+               *(*(matrix+i)+j)=(n-j-i)*(n-j-i);
+           }else{
+               *(*(matrix+i)+j)=(2*n-j-i)*(2*n-j-i);
+           }
+        }
+    }
+    //遍历该二维数组
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            printf("%d ",*(*(matrix+i)+j));
+        }
+        printf("\n");
+    }
+    //释放指针
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            free((*matrix+i)+j);
+        }
+
+    }
 }
 
--- test.c
+++ test.c
@@ -2,15 +2,11 @@
 // Created by 84539 on 2023/3/9.
 //
  #include <stdio.h>
- #define A char *
- typedef  char * B;
+
  int main(int argc, char *argv[])
  {
-     A a,b;
-     B c,d;
-     printf("a=%ld\n",sizeof(a));
-     printf("b=%ld\n",sizeof(b));
-     printf("c=%ld\n",sizeof(c));
-     printf("d=%ld\n",sizeof(d));
+
+     printf("%d",5%8);
+
      return 0;
  }
