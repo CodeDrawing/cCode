b09d228e5929dab749afc08a5ec11a175afac8df
zx <845391508@qq.com>
2023-03-19 10:39:43 +0800

    Day21

--- Day22_leecode_1614.c
+++ Day22_leecode_1614.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
-
+int maxDepth(char * s){
+    char *ps=s;
+    int current=0;
+    int max=0;
+    while (*ps!=' '){
+        if(current>max){
+            max=current;
+        }
+        if(*ps=='('){
+            current++;
+        }else if(*ps==')'){
+            current--;
+        }
+        ps++;
+    }
+    return max;
+}
 int main(){
-
+    char s[100]="(1)+((2))+(((3)))";
+    int result=maxDepth(s);
+    printf("%d\n",result);
 }
 
